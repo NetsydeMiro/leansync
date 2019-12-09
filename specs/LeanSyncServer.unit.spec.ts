556ff884bd7880384609787fe45052d4e5d996d2
NetsydeMiro <miro@netsyde.com>
2019-12-09 17:38:22 -0500

    New tests.  Added vscode config

--- specs/LeanSyncServer.unit.spec.ts
+++ specs/LeanSyncServer.unit.spec.ts
@@ -1,7 +1,7 @@
 import { LeanSyncServer, LeanSyncServerConfig, ConflictResolutionStrategy } from '../src/LeanSyncServer'
 import { Note, newNote } from '../support/Note'
 
-function mockConfig(): LeanSyncServerConfig<Note> {
+function mockConfig(conflictResolutionStrategy: ConflictResolutionStrategy<Note> = 'takeClient'): LeanSyncServerConfig<Note> {
     let config: LeanSyncServerConfig<Note> = {
         entityKey: (note) => note.id,
         entityLastUpdated: (note) => note.updatedAt,
@@ -11,7 +11,7 @@ function mockConfig(): LeanSyncServerConfig<Note> {
         getServerEntitiesSyncedSince: jest.fn().mockReturnValue([]),
         updateServerEntity: jest.fn(),
         createServerEntity: jest.fn(),
-        conflictResolutionStrategy: 'lastUpdated'
+        conflictResolutionStrategy
     }
     return config
 }
@@ -59,14 +59,16 @@ describe('LeanSyncServer', () => {
         expect(syncResult.syncedEntities[0].newKey).toBe(newGuidId)
     })
 
-    it.skip('Updates an existing entity', async () => {
-        let config: LeanSyncServerConfig<Note> = mockConfig()
+    it('Updates an existing server entity under takeClient resolution strategy', async () => {
+        let config: LeanSyncServerConfig<Note> = mockConfig('takeClient')
 
         let leanSync = new LeanSyncServer(config)
-        let clientNote = newNote('Note 1')
-
-        let serverNote = Object.assign({}, clientNote)
+        let serverNote = newNote('Note 1')
         serverNote.syncedAt = new Date()
+
+        let clientNote = Object.assign({}, serverNote)
+        clientNote.text = 'Updated Note'
+        clientNote.updatedAt = new Date()
 
         let mockGet = jest.fn().mockReturnValue([serverNote])
         config.getServerEntities = mockGet
@@ -84,4 +86,60 @@ describe('LeanSyncServer', () => {
         expect(syncResult.syncedEntities[0].entity).toBe(clientNote)
         expect(syncResult.syncedEntities[0].newKey).toBeUndefined()
     })
+
+    it('Updates an existing server entity under lastUpdated resolution strategy', async () => {
+        let config: LeanSyncServerConfig<Note> = mockConfig('lastUpdated')
+
+        let leanSync = new LeanSyncServer(config)
+        let serverNote = newNote('Note 1')
+        serverNote.syncedAt = new Date()
+
+        let clientNote = Object.assign({}, serverNote)
+        clientNote.text = 'Updated Note'
+        clientNote.updatedAt = new Date()
+
+        let mockGet = jest.fn().mockReturnValue([serverNote])
+        config.getServerEntities = mockGet
+
+        let syncResult = await leanSync.sync([clientNote])
+
+        let mockUpdate = config.updateServerEntity as jest.Mock
+
+        // existing entity is updated on server side
+        expect(mockUpdate.mock.calls.length).toBe(1)
+        expect(mockUpdate.mock.calls[0][0]).toBe(clientNote)
+
+        // client is informed of sync
+        expect(syncResult.syncedEntities.length).toBe(1)
+        expect(syncResult.syncedEntities[0].entity).toBe(clientNote)
+        expect(syncResult.syncedEntities[0].newKey).toBeUndefined()
+    })
+
+    it.skip('Informs client of required update under takeServer resolution strategy', async () => {
+        let config: LeanSyncServerConfig<Note> = mockConfig('takeServer')
+
+        let leanSync = new LeanSyncServer(config)
+        let serverNote = newNote('Note 1')
+        serverNote.syncedAt = new Date()
+
+        let clientNote = Object.assign({}, serverNote)
+        clientNote.text = 'Updated Note'
+        clientNote.updatedAt = new Date()
+
+        let mockGet = jest.fn().mockReturnValue([serverNote])
+        config.getServerEntities = mockGet
+
+        let syncResult = await leanSync.sync([clientNote])
+
+        let mockUpdate = config.updateServerEntity as jest.Mock
+
+        // existing entity is NOT updated on server side
+        expect(mockUpdate.mock.calls.length).toBe(0)
+
+        // client is informed of sync
+        expect(syncResult.syncedEntities.length).toBe(1)
+        expect(syncResult.syncedEntities[0].entity).toBe(serverNote)
+        expect(syncResult.syncedEntities[0].newKey).toBeUndefined()
+    })
+
 })
